b3a3f6107d21d477a9b530125cf99fb8ba4a2615
jeffrey-xue <jeffxue269@gmail.com>
2026-09-19 06:54:45 +0000

    rafactor playeroverlay.jsx (via LLM), paramtertize Positions to accept variable team size. Trim trailing empty names

--- src/config/Positions.js
+++ src/config/Positions.js
@@ -22,30 +22,38 @@ const spacedYCoords = (n) => {
   return ys;
 }
 
-const Positions = {
-  DEFAULT_POSITIONS: [
-    spacedYCoords(5).map(e=> ({top: e, left: xPadding})),
-    spacedYCoords(5).map(e=> ({top: e, right: xPadding})),
-  ],
-  BENCH_PLAYER_SELECTED_POSITIONS: [
-    spacedYCoords(4).map(e=> ({top: e, left: xPadding})),
-    spacedYCoords(4).map(e=> ({top: e, right: xPadding})),
-  ],
-  FOCUSED_PLAYER_SELECTED_POSITIONS: [
-    {width: focusedSize, height: focusedSize, top: height / 2 - focusedSize / 2,  left: focusedXPadding},
-    {width: focusedSize, height: focusedSize, top: height / 2 - focusedSize / 2, right: focusedXPadding},
-  ],
-  BENCH_PLAYER_GAME_POSITIONS: [
-    spacedYCoords(4).map(e=> ({top: e, left: -xPadding - defaultSize})),
-    spacedYCoords(4).map(e=> ({top: e, right: -xPadding - defaultSize})),
-  ],
-  FOCUSED_PLAYER_GAME_POSITIONS: [
-    {width: focusedSize, height: focusedSize, top: height - gameSceneYPadding - focusedSize,  left: gameSceneXPadding},
-    {width: focusedSize, height: focusedSize, top: height - gameSceneYPadding - focusedSize,  right: gameSceneXPadding},
-  ],
-  ALL_HIDDEN_GAME_POSITIONS: [
-    spacedYCoords(5).map(e=> ({top: e, left: -xPadding - defaultSize})),
-    spacedYCoords(5).map(e=> ({top: e, right: -xPadding - defaultSize})),
-  ]
+
+const createPositions = (teamSizes = [5, 5]) => {
+  const [leftSize, rightSize] = teamSizes
+  const leftBench = leftSize - 1
+  const rightBench = rightSize - 1
+
+  return {
+    DEFAULT_POSITIONS: [
+      spacedYCoords(leftSize).map(e => ({ top: e, left: xPadding })),
+      spacedYCoords(rightSize).map(e => ({ top: e, right: xPadding })),
+    ],
+    BENCH_PLAYER_SELECTED_POSITIONS: [
+      spacedYCoords(leftBench).map(e => ({ top: e, left: xPadding })),
+      spacedYCoords(rightBench).map(e => ({ top: e, right: xPadding })),
+    ],
+    FOCUSED_PLAYER_SELECTED_POSITIONS: [
+      { width: focusedSize, height: focusedSize, top: height / 2 - focusedSize / 2, left: focusedXPadding },
+      { width: focusedSize, height: focusedSize, top: height / 2 - focusedSize / 2, right: focusedXPadding },
+    ],
+    BENCH_PLAYER_GAME_POSITIONS: [
+      spacedYCoords(leftBench).map(e => ({ top: e, left: -xPadding - defaultSize })),
+      spacedYCoords(rightBench).map(e => ({ top: e, right: -xPadding - defaultSize })),
+    ],
+    FOCUSED_PLAYER_GAME_POSITIONS: [
+      { width: focusedSize, height: focusedSize, top: height - gameSceneYPadding - focusedSize, left: gameSceneXPadding },
+      { width: focusedSize, height: focusedSize, top: height - gameSceneYPadding - focusedSize, right: gameSceneXPadding },
+    ],
+    ALL_HIDDEN_GAME_POSITIONS: [
+      spacedYCoords(leftSize).map(e => ({ top: e, left: -xPadding - defaultSize })),
+      spacedYCoords(rightSize).map(e => ({ top: e, right: -xPadding - defaultSize })),
+    ],
+  }
 }
-export default Positions
+
+export default createPositions
